0f5ea4c13c52b2258c91e0a43d2426fadd896ae9
georgiygusev <Ge0rgiusGus@yandex.ru>
2024-07-22 19:31:55 +0300

    initial

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/neiasit/auth-library
+
+go 1.22.5
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/lestrrat-go/jwx v1.2.29
+	google.golang.org/grpc v1.65.0
+)
+
+require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+)
---
+++ pkg/provider/provider.go
@@ -0,0 +1,36 @@
+// It's legacy not usable code from this repository - https://github.com/BobrePatre/ProjectTemplate/tree/main/internal/providers/web_auth_provider
+// Use this template to write perfect convenient auth provider library
+// TODO: redesign the authentication provider
+
+package provider
+
+import (
+	"context"
+	"github.com/golang-jwt/jwt"
+	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/neiasit/auth-library/pkg/models"
+	"google.golang.org/grpc"
+	"net/http"
+)
+
+type Middleware interface {
+	GrpcUnaryServerInterceptor() grpc.UnaryServerInterceptor
+	GrpcStreamServerInterceptor() grpc.StreamServerInterceptor
+	HttpMiddleware() http.HandlerFunc
+}
+
+type AuthProvider interface {
+	VerifyToken(ctx context.Context, tokenString string) (*jwt.Token, error)
+	TokenKeyfunc(ctx context.Context) jwt.Keyfunc
+	FetchJwkSet(ctx context.Context) (jwk.Set, error)
+	IsUserHaveRoles(roles []string, userRoles []string) bool
+	SerializeJwkSet(key jwk.Set) (string, error)
+	DeserializeJwkSet(serializedKey string) (jwk.Set, error)
+	Authorize(ctx context.Context, tokenString string, roles []string) (models.UserDetails, error)
+	CheckSsoConnection(ctx context.Context) error
+}
+
+const (
+	JwkKeySet      = "jwk-set"
+	UserDetailsKey = "userDetails"
+)
